c69050e52903e0640710203a94168517a8226ad5
pombadev <11157420+pombadev@users.noreply.github.com>
2024-07-03 22:27:30 +0545

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Context, context, context!: Add more stuffs to context menu
+
+Currently included menus:
+
+- Find selection in Files
+
+## Credits
+
+<a href="https://www.flaticon.com/free-icons/context" title="context icons">Context icons created by Aleksei Ryazancev - Flaticon</a>
---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import {type ExtensionContext, window, commands} from 'vscode';
+
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('moar-ctx.findSelectionInFiles', () => {
+
+    const selection = window.activeTextEditor?.document.getText(window.activeTextEditor?.selection);
+
+    commands.executeCommand("workbench.action.findInFiles", {
+      query: selection
+    });
+	});
+
+	context.subscriptions.push(disposable);
+}
