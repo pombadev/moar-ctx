8500a5cc39bc434bca5493fed834ef5eafd1a29b
pombadev <11157420+pombadev@users.noreply.github.com>
2024-07-03 22:53:03 +0545

    format

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import {type ExtensionContext, window, commands} from 'vscode';
+import { type ExtensionContext, window, commands } from 'vscode';
 
 export function activate(context: ExtensionContext) {
   const disposable = commands.registerCommand('moar-ctx.findSelectionInFiles', () => {
@@ -8,7 +8,7 @@ export function activate(context: ExtensionContext) {
     commands.executeCommand("workbench.action.findInFiles", {
       query: selection
     });
-	});
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
